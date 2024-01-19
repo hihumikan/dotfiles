0b253b1230068e99700ca034eb29f7fcc895e572
hihumikan <musecharo@gmail.com>
2024-01-19 19:56:51 +0900

    Update advln.sh

--- advln.sh
+++ advln.sh
@@ -4,4 +4,4 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 
-echo "--- Link dotfiles is fuxxing Done!  ---"
+echo "--- Link dotfiles is Done!  ---"
