4d95950c7abcfdf74b64d536c26170c699916036
hihumikan <a@qqey.net>
2024-02-15 18:40:46 +0900

    dotfilesのパスを修正***

--- host/full-ln.sh
+++ host/full-ln.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ln -sf ~/dotfiles/host/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/host/config/starship.toml ~/.config/starship.toml
-ln -sf ~/dotfiles/host/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 
 echo "--- Link dotfiles is Done!  ---"
