a10e47fd9407679ddbf2a13cb4403a8db13f4022
hihumikan <a@qqey.net>
2024-02-15 19:02:10 +0900

    Brewfileとfull-ln.shに変更を加えました

--- host/full-ln.sh
+++ host/full-ln.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p ~/.config/sheldon
+
 ln -sf ~/dotfiles/host/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
--- host/install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update && apt -y upgrade
-
-echo "Installing Volta"
-curl https://get.volta.sh | bash
-
-echo "Installing build-essential"
-sudo apt install build-essential
-
-brew bundle 
